3fab819a2016e8c0e7b2b52fbc239eacb00a8607
nilesh01011 <nrrathod223@gmail.com>
2023-10-15 10:38:03 +0530

    company experience description add and resume pdf update and new projects added

--- pages/api/Companys.js
+++ pages/api/Companys.js
@@ -4,20 +4,23 @@ export const companyName = [
         year: '2022 - 2023',
         months: 'Present',
         company: 'Mahindra & Mahindra Ltd',
-        // subtitle: 'Software-Developer-Interns',
         subtitle: 'Front End Developer',
         contents: [
             {
                 id: 1,
-                text: 'Managed frontend codebase with Next.js and Created a reusable UI components with Tailwind CSS with Optimized.'
+                text: 'Managed frontend codebase built with Next.js and created reusable UI components with Tailwind CSS or either SCSS to improve development efficiency.'
             },
             {
                 id: 2,
-                text: 'Learned Salesforce development and Built apps and components with Salesforce Lightning Web Components LWC Studio.'
+                text: 'Optimized application performance by integrating modern frameworks and libraries.'
             },
             {
                 id: 3,
-                text: 'Learned Sanity headless CMS and Implemented as backend for projects.'
+                text: 'Implemented real-time data fetching from APIs using Salesforce Lightning Web Components (LWC).'
+            },
+            {
+                id: 4,
+                text: 'Implemented Sanity as a headless CMS backend for multiple projects. Gained experience with Sanity for use as a CMS in enterprise environments.'
             },
         ],
         links: 'https://auto.mahindra.com/'
@@ -31,15 +34,19 @@ export const companyName = [
         contents: [
             {
                 id: 1,
-                text: 'Managed frontend codebase with Next.js and Created a reusable UI components with Tailwind CSS with Optimized.'
+                text: 'Managed frontend development using NextJS and Tailwind CSS.'
             },
             {
                 id: 2,
-                text: 'Created a reused components to optimized the code and speed of web apps.'
+                text: 'Created reusable UI components to optimize code and improve web app performance.'
             },
             {
                 id: 3,
-                text: 'And also created a UI components for the custom design.'
+                text: 'Developed custom UI components to match product design system specifications.'
+            },
+            {
+                id: 4,
+                text: 'Implemented UI with NextJS and Tailwind CSS following reusable components architecture.'
             },
         ],
         links: 'https://www.ajackus.com/'
